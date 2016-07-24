255d87a273dee0ae1f3c96917fd789e4e4f324cb
teleblue <b.anersazfa@gmail.com>
2016-07-24 21:46:36 +0430

    Update joining.lua

--- plugins/joining.lua
+++ plugins/joining.lua
@@ -1,12 +1,12 @@
 local function run(msg, matches)
     if matches[1] == "support" then
  local user = "user#id"..msg.from.id
- local fachat = "chat#id"..123229873
+ local fachat = "chat#id"..80882965
  chat_add_user(fachat, user, ok_cb, false)
     end
     if matches[1] == "ساپورت" then
  local user = "user#id"..msg.from.id
- local devchat = "chat#id"..123229873
+ local devchat = "chat#id"..80882965
  chat_add_user(devchat, user, ok_cb, false)
     end
 end
