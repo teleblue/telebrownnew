b31a12a8999d7c68ec2dc90a065f6b77a7560665
teleblue <b.anersazfa@gmail.com>
2016-07-24 21:51:21 +0430

    Update time2.lua

--- plugins/time2.lua
+++ plugins/time2.lua
@@ -12,7 +12,7 @@ function run(msg, matches)
 local url , res = http.request('http://api.gpmod.ir/time/')
 if res ~= 200 then return "No connection" end
 local jdat = json:decode(url)
-local text = '⌚ ساعت '..jdat.FAtime..' \n📅 امروز '..jdat.FAdate..' میباشد.\n    ----\n⏰ '..jdat.ENtime..'\n📆 '..jdat.ENdate.. '\n@TeleUmbrella_team\nThx To GpMod'
+local text = '⌚ ساعت '..jdat.FAtime..' \n📅 امروز '..jdat.FAdate..' میباشد.\n    ----\n⏰ '..jdat.ENtime..'\n📆 '..jdat.ENdate.. '\n@TeleUmbrella_team\nThx To TeleUmbrella'
 return text
 end
 return {
